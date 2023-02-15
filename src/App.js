bd1fc188c81b989d34b6d3f67b8bb0326c574d45
styu12 <styu12@naver.com>
2023-02-15 20:52:56 +0900

    [feat] Route Setting / react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import CreatePost from './CreatePost';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <Header />
+//       <CreatePost />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <CreatePost />
-      <Footer />
+      <BrowserRouter>
+          <Header />
+          <Routes>
+            {/* home */}
+            {/* <Route  path="/" element={<Home />} /> */}
+            {/* craete post */}
+            <Route  path="/post/create" element={<CreatePost />} />
+            {/* edit post */}
+            {/* <Route  path="/:postId/edit" element={<EditPost />} /> */}
+            {/* sign up */}
+            {/* <Route  path="/signup" element={<Signup />} />/ */}
+          </Routes>
+          <Footer />
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
