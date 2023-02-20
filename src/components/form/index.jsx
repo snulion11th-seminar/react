89b55eb4166fce67b438842dde07d6e3c569d177
jihyune <rlawlcns96@gmail.com>
2023-02-20 18:12:41 +0900

    feat: post detail

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,60 +1,68 @@
 export const SignUpForm = () => {
-    return (
-        <div>
-            <form className="flex flex-col">
-                <label required for="email">
-                    *email:
-                </label>
-                <input type="text" name="email" />
-                <label required for="password">
-                    *passoword:
-                </label>
-                <input type="text" name="password" />
-                <label for="confirm_password">*passoword 확인: </label>
-                <input type="text" name="confirm_password" />
-                <label for="name">이름:</label>
-                <input type="text" name="username" />
-                <label for="college">대학: </label>
-                <input type="text" name="college" />
-                <label for="major">전공: </label>
-                <input type="text" id="major" />
-                <button type="submit" text="Sign up !" />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form className="flex flex-col">
+        <label required for="email">
+          *email:
+        </label>
+        <input type="text" name="email" />
+        <label required for="password">
+          *passoword:
+        </label>
+        <input type="text" name="password" />
+        <label for="confirm_password">*passoword 확인: </label>
+        <input type="text" name="confirm_password" />
+        <label for="name">이름:</label>
+        <input type="text" name="username" />
+        <label for="college">대학: </label>
+        <input type="text" name="college" />
+        <label for="major">전공: </label>
+        <input type="text" id="major" />
+        <button type="submit" text="Sign up !" />
+      </form>
+    </div>
+  );
 };
 
 export const SignInForm = () => {
-    return (
-        <div>
-            <form className="flex flex-col">
-                <label required for="email">
-                    *email:
-                </label>
-                <input type="text" name="email" />
-                <label required for="password">
-                    *passoword:
-                </label>
-                <input type="text" name="password" />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form className="flex flex-col">
+        <label required for="email">
+          *email:
+        </label>
+        <input type="text" name="email" />
+        <label required for="password">
+          *passoword:
+        </label>
+        <input type="text" name="password" />
+      </form>
+    </div>
+  );
 };
 
-export const PostForm = () => {
-    return (
-        <form id="create-post-wrapper">
-            <h3 id="create-post-header">new post</h3>
-            <span id="title">title</span>
-            <input type="text" id="create-post-title-input" />
-            <span id="content">content</span>
-            <textarea
-                name="content"
-                cols="30"
-                rows="10"
-                id="create-post-content-input"
-            ></textarea>
-            <button id="create-post-btn">제출하기</button>
-        </form>
-    );
+export const PostForm = ({
+  onSubmit,
+  updateTitle,
+  updateContent,
+  updateTag,
+}) => {
+  return (
+    <form className="flex flex-col py-50 bg-slate-500" onSubmit={onSubmit}>
+      <h3 id="create-post-header">new post</h3>
+      <span id="title">title</span>
+      <input type="text" id="create-post-title-input" onChange={updateTitle} />
+      <span id="content">content</span>
+      <textarea
+        name="content"
+        cols="30"
+        rows="10"
+        id="create-post-content-input"
+        onChange={updateContent}
+      ></textarea>
+      <span id="content">tag</span>
+      <input type="text" id="create-post-tag-input" onChange={updateTag} />
+      <button id="create-post-btn">제출하기</button>
+    </form>
+  );
 };
--- src/routes/PostDetail.jsx
+++ src/routes/PostDetail.jsx
@@ -1,13 +1,23 @@
 import Comments from "../components/Comments";
 
-const PostDetail = () => {
-    return (
-        <div>
-            <div>PostDetail</div>
-            <Comments />
+const PostDetail = ({ title, content, tags }) => {
+  //TODO : tags="고양이, 야옹야옹, 강아지, 멍멍" parsing
+  tags = ["고양이", "야옹야옹"];
+  return (
+    <div>
+      <div>Detail</div>
+      <div className="mt-40 bg-slate-500">
+        <div className="post-detail-wrapper">
+          <div className="post-detail-title">제목 : {title}</div>
+          <div className="post-detail-content">내용 : {content}</div>
+          {tags.map((tag) => (
+            <div className="post-detail-tag">태그 : {tag}</div>
+          ))}
         </div>
-        
-    )
+        <Comments />
+      </div>
+    </div>
+  );
 };
 
 export default PostDetail;
