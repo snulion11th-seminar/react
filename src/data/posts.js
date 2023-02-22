4f5b51f21102092414d45b250726439f040c974e
styu12 <styu12@naver.com>
2023-02-22 16:54:24 +0900

    [FEAT] tag dynamic search bar & tag filtering

--- src/data/posts.js
+++ src/data/posts.js
@@ -6,28 +6,28 @@ const  posts = [
         title: "#1 post",
         content: "My Number one post content",
         summary: "number one summary",
-        tags: ["tag1", "tag2", "tag3"],
+        tags: ["멋사", "개발자", "서울대"],
     },
     {
         id: 2,
         title: "#2 post",
         content: "My Number two post content",
         summary: "number two summary",
-        tags: ["tag11", "tag12", "tag13"],
+        tags: ["아기사자", "서울대", "멋사"],
     },
     {
         id: 3,
         title: "#3 post",
         content: "My Number three post content",
         summary: "number three summary",
-        tags: ["tag21", "tag22", "tag23"],
+        tags: ["웹개발", "동아리", "11기"],
     },
     {
         id: 4,
         title: "#4 post",
         content: "My Number four post content",
         summary: "number four summary",
-        tags: ["tag31", "tag32", "tag33"],
+        tags: ["아기사자", "프로그래밍", "웹개발"],
     }, 
 ];
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,18 +1,77 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { SmallPost } from "../components/Posts";
 import posts from "../data/posts";
 
 
 const Home = () => {
+    const [tags, setTags] = useState([]);
+    const [searchTags, setSearchTags] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+    const [postList, setPostList] = useState(posts);
+
+    useEffect(() => {
+        const tagList = posts.reduce((acc, post) => {
+            for (let tag of post.tags) {
+                acc.add(tag);
+            }
+            return acc;
+        }, new Set());
+        setTags([...tagList]);
+        setSearchTags([...tagList]);
+    }, []);
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        const newTags = tags.filter((tag) => tag.includes(value));
+        setSearchTags(newTags);
+    }
+
+    const handleTagFilter = (e) => {
+        const { innerText } = e.target;
+        const activeTag = innerText.substring(1);
+        setSearchValue(activeTag);
+        const newPosts = posts.filter((post) => post.tags.includes(activeTag));
+        setPostList(newPosts);
+    }
+
     return (
         <div>
-            <div className="flex flex-col justify-center items-center h-72 bg-slate-600 mb-5">
+            <div className="flex flex-col justify-center items-center h-72 bg-[url('https://www.codelion.net/codelion_thumb.jpg')] bg-center bg-cover mb-5">
                 <h1 className="uppercase text-6xl mb-3">my blog</h1>
-                <input type="text" placeholder="TAG SEARCH" className="border-2 border-orange-400 outline-none rounded-2xl text-center py-2 px-20 text-orange-400" />
+                <input 
+                    type="text" 
+                    placeholder="Tag Search" 
+                    onChange={handleChange}
+                    className="border border-orange-400 outline-none rounded-2xl text-center py-2 px-20 text-orange-400 bg-transparent" />
+                <div className="flex mt-5">
+                      {searchTags.map((tag) => {
+                        if (tag === searchValue) {
+                            return (
+                                <button 
+                                    key={tag} 
+                                    className="tag active mr-2"
+                                    onClick={handleTagFilter}>
+                                        #{tag}
+                                </button>
+                            )
+                        }   else {
+                            return (
+                                <button 
+                                   key={tag} 
+                                   className="tag mr-2"
+                                   onClick={handleTagFilter}>
+                                       #{tag}
+                               </button>
+                               )
+                        }
+                    }
+                )}
+                </div>
             </div>
 
             <div className="flex flex-wrap mt-10 justify-center">
-                {posts.map((post) => (
+                {postList.map((post) => (
                     <SmallPost key={post.id} post={post} />
                     )
                 )}
