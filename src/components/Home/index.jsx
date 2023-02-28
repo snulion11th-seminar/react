feaecbef79321125638ba8472097757a620433ac
jihyune <rlawlcns96@gmail.com>
2023-02-28 23:32:53 +0900

    fix: remove parameter index in mapping post

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,27 +6,27 @@ import Post from "../Post";
 const Home = () => {
   const posts = [
     {
-      id : 1,
+      id: 1,
       title: "날씨가 좋네요",
       content: "비 오는데요?",
     },
-    { 
-      id : 2,
+    {
+      id: 2,
       title: "멋쟁이",
       content: "사자~~",
     },
     {
-      id : 3,
+      id: 3,
       title: "쿠쿠루삥뽕",
       content: "안녕",
     },
     {
-      id : 4,
+      id: 4,
       title: "오늘 뒷풀이 가는 사람?",
       content: "저요!",
     },
     {
-      id : 5,
+      id: 5,
       title: "오늘 할 일",
       content: "아침에 일어나기",
     },
@@ -35,9 +35,9 @@ const Home = () => {
   return (
     <div className="home-wrapper">
       <div className="posts-outer-wrapper">
-      {posts.map((post, index) => (
-        <Post key={post.id} title={post.title} content={post.content}/>
-      ))}
+        {posts.map((post) => (
+          <Post key={post.id} title={post.title} content={post.content} />
+        ))}
       </div>
       <Tags />
     </div>
