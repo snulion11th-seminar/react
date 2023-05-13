e30347867096dd8a5ea8ff4f966e806f4edd41ef
whwoohw <woojoo1114@naver.com>
2023-05-13 15:44:08 +0900

    [fix] signIn API

--- src/routes/SignInPage.jsx
+++ src/routes/SignInPage.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import { SignInForm } from "../components/Form";
+import { signIn } from "../apis/api";
 
 const SignInPage = () => {
-	const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
 
-  const handleSignInSubmit = () => {
-	console.log(formData);
-    alert("로그인 완 료!");
-	// add api call for sign in here
+  const handleSignInSubmit = (e) => {
+    e.preventDefault();
+    signIn(formData);
   };
 
   return (
     <div className="flex flex-col items-center w-1/2">
       <h3 className=" font-bold text-4xl">Sign In</h3>
-      <SignInForm 
-		formData={formData} 
-		setFormData={setFormData} 
-		handleSignInSubmit={handleSignInSubmit} 
-	  />
+      <SignInForm
+        formData={formData}
+        setFormData={setFormData}
+        handleSignInSubmit={handleSignInSubmit}
+      />
     </div>
   );
 };
