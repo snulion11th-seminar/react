1283e4c74191921dbe836c7763494c5382c3995f
isuh88 <isuh88@snu.ac.kr>
2023-05-16 01:09:23 +0900

    [Feat] comments added without appropriate delete function

--- src/components/Comments/CommentElement.jsx
+++ src/components/Comments/CommentElement.jsx
@@ -1,29 +1,59 @@
-const CommentElement = (props) => {
+import { useState } from "react";
+
+const CommentElement = ({ comment, commentList, setCommentList }) => {
   // TODO : props 받기
   // TODO : 수정하는 input 내용 관리
-
+  const [isEditting, setIsEditting] = useState(false);
+  const [commentData, setCommentData] = useState(comment.content);
   // comment created_at 전처리
-  const date = new Date(comment.created_at);
+  const date = new Date();
   const year = date.getFullYear();
   let month = date.getMonth() + 1;
   month = month < 10 ? `0${month}` : month;
   let day = date.getDate();
   day = day < 10 ? `0${day}` : day;
+  const handleEditComment = (e) => {
+    setCommentData(e.target.value);
+  };
+  const deleteComment = (id) => {
+    console.log(id);
+    setCommentList(commentList.filter((comment) => comment.id !== id));
+  };
 
-  <div className="w-full flex justify-between gap-1 mb-2">
-    <div className="w-3/4">
-      // 수정중일때와 아닐때를 다르게 보여줘야겠지
-      {수정중 ? <input /> : <p>{내용}</p>}
-      // 날짜
-      <span className="text-base mr-1 text-gray-300">
-        {year}.{month}.{day}
-      </span>
-      // 수정, 삭제버튼
+  return (
+    <div className="w-full flex justify-between gap-1 mb-2">
+      <div className="w-3/4">
+        {/* 수정중일때와 아닐때를 다르게 보여줘야겠지 */}
+        {isEditting ? (
+          <input
+            className="input"
+            value={commentData}
+            onChange={handleEditComment}
+          />
+        ) : (
+          <p>{commentData}</p>
+        )}
+        {/* 날짜 */}
+        <span className="text-base mr-1 text-gray-300">
+          {year}.{month}.{day}
+        </span>
+      </div>
+      {/* 수정, 삭제버튼 */}
       <div className="w-1/4 flex flex-row-reverse items-center">
-        // delete 버튼은 수정이 아닐때만 보이게 해줘
+        {/* delete 버튼은 수정이 아닐때만 보이게 해줘 */}
+        {isEditting ? (
+          <button onClick={() => setIsEditting(false)}>Done</button>
+        ) : (
+          <div>
+            <button className="mr-3" onClick={() => setIsEditting(true)}>
+              Edit
+            </button>
+            <button onClick={() => deleteComment(comment.id)}>Del</button>
+          </div>
+        )}
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default CommentElement;
--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -4,17 +4,60 @@ import CommentElement from "./CommentElement";
 
 const Comment = () => {
   // TODO 1: 가짜 comments 불러와서 관리해야겟즤
-
+  const [commentList, setCommentList] = useState(comments);
   // TODO 2: comment추가하는 input 관리해줘야겟지
-
+  const [formData, setFormData] = useState({
+    content: "",
+  });
+  const handleFormData = (e) => {
+    const value = e.target.value;
+    setFormData({ ...formData, content: value });
+  };
   // TODO 3: comment Form 제출됐을때 실행되는 함수 만들어줘
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    const newComment = {
+      id: commentList[commentList.length - 1].id + 1,
+      content: formData.content,
+      created_at: "",
+      post: 1,
+      author: {
+        id: 1,
+        username: "user1",
+      },
+    };
+    console.log(newComment);
+    setCommentList([...commentList, newComment]);
+    setFormData({ content: "" });
+  };
 
-  // TODO 4: commet Delete 하는 함수 만들어죠
+  console.log(commentList);
   return (
     <div className="w-full mt-5 self-start">
       <h1 className="text-3xl font-bold mt-5 mb-3">Comments</h1>
-      // commentElement // <CommentElement /> 가 comment마다 반복시켜야즤
-      <form>// TODO 2-3 : comment 추가하는 comment form 만들어주기</form>
+      <div>
+        {commentList.map((comment) => (
+          <CommentElement
+            comment={comment}
+            commentList={commentList}
+            setCommentList={setCommentList}
+          />
+        ))}
+      </div>
+      {/* commentElement // <CommentElement /> 가 comment마다 반복시켜야즤 */}
+      <form
+        className="flex justify-center items-center mt-10 gap-2"
+        onSubmit={handleCommentSubmit}
+      >
+        <input
+          required
+          className="input w-3"
+          placeholder="댓글을 입력해주세요"
+          value={formData.content}
+          onChange={handleFormData}
+        ></input>
+        <button className="button">Comment</button>
+      </form>
     </div>
   );
 };
