682fa5eb9628f48d1c4942a1567993b40e2f31d9
JoeYoojin <uzinfamily@naver.com>
2023-05-15 19:43:12 +0900

    feat: week9 seminar

---
+++ src/routes/PostEditPage.jsx
@@ -0,0 +1,7 @@
+const PostEditPage = () => {
+	return (
+		<div>여기는 Post Edit 페이지!</div>
+	);
+};
+
+export default PostEditPage;
---
+++ src/routes/SignInPage.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { SignInForm } from "../components/Form";
+
+const SignInPage = () => {
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleFormData = (e) => {
+        const { id, value } = e.target;
+        setFormData({ ...formData, [id]: value });
+    };
+
+    const handleSignInSubmit = () => {
+        console.log(formData);
+        alert("로그인 완 료!");
+        // add api call for sign in here
+    };
+
+	return (
+        <div className="flex flex-col items-center w-1/2">
+        <h3 className=" font-bold text-4xl">Sign In</h3>
+
+        <SignInForm 
+				formData={formData} 
+				setFormData={setFormData} 
+				handleSignInSubmit={handleSignInSubmit} 
+			/>
+        </div>
+	);
+};
+
+export default SignInPage;
