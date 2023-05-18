5875c11d64d1831bb36188f62cd8b9fa99d833f9
heeseon <hseon326@gmail.comgit config --global user.email hseon326@gmail.comgit config --listhseon326@gmail.com>
2023-05-18 11:01:45 +0900

    week9 assignment

--- src/components/Comment/CommentElement.jsx
+++ src/components/Comment/CommentElement.jsx
@@ -1,6 +1,29 @@
-const CommentElement = (props) => {
+import { useState } from "react";
+
+export const CommentElement = ({ comment, deleteComment }) => {
   // TODO : props 받기
   // TODO : 수정하는 input 내용 관리
+  const [content, setContent] = useState(comment.content);
+  const [editInput, setEditInput] = useState(content);
+  const [isEdit, setIsEdit] = useState(false);
+  
+  const handleEdit = (e) => {
+    setEditInput(e.target.value);
+  };
+
+  const onClickEdit = () => {
+    setIsEdit(true);
+  };
+
+  const onClickDone = () => {
+    setIsEdit(false);
+    setContent(editInput);
+  };
+
+  const onClickDel = () => {
+    console.log(comment.id)
+    deleteComment(comment.id)
+  };
 
   // comment created_at 전처리
   const date = new Date(comment.created_at);
@@ -10,20 +33,50 @@ const CommentElement = (props) => {
   let day = date.getDate();
   day = day < 10 ? `0${day}` : day;
 
-  <div className="w-full flex justify-between gap-1 mb-2">
-    <div className="w-3/4">
-      // 수정중일때와 아닐때를 다르게 보여줘야겠지
-      {수정중 ? <input /> : <p>{내용}</p>}
-      // 날짜
-      <span className="text-base mr-1 text-gray-300">
-        {year}.{month}.{day}
-      </span>
-      // 수정, 삭제버튼
+  return (
+    <div className="w-full flex justify-between gap-1 mb-2">
+      <div className="w-3/4">
+        {/* // 수정중일때와 아닐때를 다르게 보여줘야겠지 */}
+        {isEdit ? (
+          <input
+            type="text"
+            placeholder={content}
+            id="comment"
+            value={editInput}
+            className="input"
+            onChange={handleEdit}
+          />
+        ) : (
+          <p>{content}</p>
+        )}
+        {/* // 날짜 */}
+        <span className="text-base mr-1 text-gray-300">
+          {year}.{month}.{day}
+        </span>
+      </div>
+      {/* // 수정, 삭제버튼 */}
       <div className="w-1/4 flex flex-row-reverse items-center">
-        // delete 버튼은 수정이 아닐때만 보이게 해줘
+        {/* // delete 버튼은 수정이 아닐때만 보이게 해줘 */}
+        {isEdit ? (
+          null
+        ) : (
+          <button className="mr-3" onClick={onClickDel}>
+            Del
+          </button>
+        )}
+        {isEdit ? <button
+            className="mr-3" onClick={onClickDone}
+          >
+            Done
+          </button> : 
+          <button
+          className="mr-3" onClick={onClickEdit}
+        >
+          Edit
+        </button>
+          }
       </div>
     </div>
-  </div>;
+  );
 };
 
-export default CommentElement;
