661694be6f566d595b18258de8789e03cc8c23d0
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-05-19 16:15:28 +0900

    week9 seminar assignment

--- src/components/Comment/CommentElement.jsx
+++ src/components/Comment/CommentElement.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 const CommentElement = (props) => {
-  // TODO : props 받기
+  // TODO : props 받기 *
   // TODO : 수정하는 input 내용 관리
+  const [isEditing, setIsEditing] = useState(false);
+  const { comment, handleCommentDelete } = props;
+  const [commentContent, setCommentContent] = useState(comment.content);
 
   // comment created_at 전처리
   const date = new Date(comment.created_at);
@@ -10,20 +15,32 @@ const CommentElement = (props) => {
   let day = date.getDate();
   day = day < 10 ? `0${day}` : day;
 
-  <div className="w-full flex justify-between gap-1 mb-2">
-    <div className="w-3/4">
-      // 수정중일때와 아닐때를 다르게 보여줘야겠지
-      {수정중 ? <input /> : <p>{내용}</p>}
-      // 날짜
-      <span className="text-base mr-1 text-gray-300">
-        {year}.{month}.{day}
-      </span>
-      // 수정, 삭제버튼
+  return (
+    <div className="w-full flex justify-between gap-1 mb-2">
+      <div className="w-3/4">
+        {isEditing ? (
+          <input
+            className="input"
+            value={commentContent}
+            onChange={(e) => setCommentContent(e.target.value)}
+          />
+        ) : (
+          <p className="text-lg mr-4">{commentContent}</p>
+        )}
+        <span className="text-base mr-1 text-gray-300">
+          {year}.{month}.{day}
+        </span>
+      </div>
       <div className="w-1/4 flex flex-row-reverse items-center">
-        // delete 버튼은 수정이 아닐때만 보이게 해줘
+        <button onClick={() => handleCommentDelete(comment.id)}>
+          {isEditing ? "" : "Del"}
+        </button>
+        <button className="mr-3" onClick={() => setIsEditing(!isEditing)}>
+          {isEditing ? "Done" : "Edit"}
+        </button>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default CommentElement;
--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,22 +1,70 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import comments from "../../data/comments";
 import CommentElement from "./CommentElement";
 
-const Comment = () => {
-  // TODO 1: comments 불러와서 저장해야겟즤
+export const Comment = ({ postId }) => {
+  const [filteredComments, setFilteredComments] = useState([]);
+  const [newInput, setNewInput] = useState("");
 
-  // TODO 2: comment추가하는 input 관리해줘야겟지
+  useEffect(() => {
+    const commentList = comments.filter(
+      (comment) => comment.post === parseInt(postId)
+    );
+    setFilteredComments(commentList);
+  }, [postId]);
 
-  // TODO 3: comment Form 제출됐을때 실행되는 함수 만들어줘
+  const handleCommentInput = (e) => {
+    let value = e.target.value;
+    setNewInput(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newComment = {
+      id: comments.length + 1,
+      content: newInput,
+      created_at: Date.now(),
+      author: {
+        id: 1,
+        username: "user1",
+      },
+    };
+    setFilteredComments([...filteredComments, newComment]);
+    setNewInput("");
+  };
 
   // TODO 4: commet Delete 하는 함수 만들어죠
+  const handleCommentDelete = (id) => {
+    setFilteredComments(
+      filteredComments.filter((comment) => comment.id !== id)
+    );
+  };
+
   return (
     <div className="w-full mt-5 self-start">
       <h1 className="text-3xl font-bold mt-5 mb-3">Comments</h1>
-      // commentElement // <CommentElement /> 가 comment마다 반복시켜야즤
-      <form>// TODO 2-3 : comment 추가하는 comment form 만들어주기</form>
+      {filteredComments.map((comment) => {
+        return (
+          <CommentElement
+            key={comment.id}
+            comment={comment}
+            handleCommentDelete={handleCommentDelete}
+          />
+        );
+      })}
+      <form className="flex mt-10 gap-2 justify-center items-center">
+        <input
+          type="text"
+          placeholder="댓글을 입력해주세요"
+          className="input h-14"
+          onChange={handleCommentInput}
+          value={newInput}
+          required
+        ></input>
+        <button className="button" type="submit" onClick={handleSubmit}>
+          comment
+        </button>
+      </form>
     </div>
   );
 };
-
-export default Comment;
