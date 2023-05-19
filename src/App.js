871496223c711ab8524d96f6b31e7412dd9e934b
unknownburphy <chrisandjj1218@gmail.com>
2023-05-19 23:58:15 +0900

    feat: add App.js and add Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import logo from "./logo.svg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./routes/Home";
 import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import PostCreatePage from "./routes/PostCreatePage";
+import PostEditPage from "./routes/PostEditPage";
+import SignUpPage from "./routes/SignUpPage";
+import PostDetailPage from "./routes/PostDetailPage";
+import SignInPage from "./routes/SignInPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      {/* 수정 👇🏻 */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* home */}
+          <Route path="/" element={<HomePage />} />
+          {/* craete post */}
+          <Route path="/create" element={<PostCreatePage />} />
+          {/* edit post */}
+          <Route path="/:postId/edit" element={<PostEditPage />} />
+          {/* post detail */}
+          <Route path="/:postId" element={<PostDetailPage />} />
+          {/* sign up */}
+          <Route path="/signup" element={<SignUpPage />} />
+          {/* sign up */}
+          <Route path="/signin" element={<SignInPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      {/* 수정 👆🏻 */}
     </div>
   );
 }
